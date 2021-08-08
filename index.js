b75360e144d0c62b6aed4d3fa4469baf243350be
jinkeyy <71177441+jinkeyy@users.noreply.github.com>
2021-08-09 00:19:13 +0700

    update login,register

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express()
 const bodyParser = require("body-parser")
 const bcrypt = require("bcryptjs")
 const mongoose = require('mongoose');
+const config = require('./src/config.json');
+mongoose.connect(config.MongoDb.connectionString, { useNewUrlParser: true })
 
-mongoose.connect('mongodb://localhost:27017/movie_db', { useNewUrlParser: true })
 
-const port = 3500
+
+const port = config.App.localhost
 app.listen(port,()=>{
     console.log("Cổng "+port)
 })
@@ -16,3 +18,4 @@ const register = require("./src/routes/route.register")
 //// Thềm route vào dưới đây hic
 app.use(login)
 app.use(register)
+
