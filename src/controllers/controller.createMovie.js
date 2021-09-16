3d2d1c514a0d3c4e636f0a907f582d3999a1a9a3
jinkeyy <71177441+jinkeyy@users.noreply.github.com>
2021-09-16 16:57:11 +0700

    fix-actors

--- src/controllers/controller.createMovie.js
+++ src/controllers/controller.createMovie.js
@@ -1,29 +1,30 @@
 const Movie = require("../models/models.movie.js")
 const uploadFile = require("../modules/uploadfile")
 
-createMovieController = async (reqs,res)=>{
+createMovieController = async (reqs, res) => {
     const type = reqs.body.typemovie.split(",")
-    if(!reqs.files) return res.status(400).send({"error":"Không tìm thấy file ảnh"})
-    if(!reqs.files.image) return res.status(400).send({"error":"Không tìm thấy file ảnh"})
+    const actors = reqs.body.actors.split(",")
+    if (!reqs.files) return res.status(400).send({ "error": "Không tìm thấy file ảnh" })
+    if (!reqs.files.image) return res.status(400).send({ "error": "Không tìm thấy file ảnh" })
     const imagelink = await uploadFile(reqs.files.image)
-    if(reqs.body.moviename && reqs.body.movielink){
+    if (reqs.body.moviename && reqs.body.movielink) {
         const newMovie = new Movie();
         newMovie.moviename = reqs.body.moviename
         newMovie.movielink = reqs.body.movielink
         newMovie.imagelink = imagelink
         newMovie.trailerlink = reqs.body.trailerlink
         newMovie.description = reqs.body.description
-        newMovie.actors = reqs.body.actors
+        newMovie.actors = actors
         newMovie.national = reqs.body.national
         newMovie.typemovie = type
-        try{
+        try {
             const Movie = await newMovie.save()
-            res.status(200).send({"message":"Thêm phim mới thành công"});
-        }catch(err){
+            res.status(200).send({ "message": "Thêm phim mới thành công" });
+        } catch (err) {
             res.status(400).send(err);
         }
-    }else{
-        res.status(400).send({"error":"Thiếu đầu vào"});
+    } else {
+        res.status(400).send({ "error": "Thiếu đầu vào" });
     }
 
 }
--- src/controllers/controller.updateMovie.js
+++ src/controllers/controller.updateMovie.js
@@ -4,25 +4,32 @@ const app = express()
 
 updateMovieController = async (reqs,res)=>{
     const type = reqs.body.typemovie.split(",")
-    const updateImage = false
-    if(reqs.body.moviename && reqs.body.movielink && reqs.body.imagelink){
+    const actors = reqs.body.actors.split(",")
+    if(reqs.files == null) {
+        reqs.files = {}
+    }
+    if(reqs.files.background){
+        console.log("có ảnh background");
+    }
+    if(reqs.body.moviename && reqs.body.movielink){
         const newMovie = {};
         newMovie.moviename = reqs.body.moviename
         newMovie.movielink = reqs.body.movielink
-        if(updateImage){
-            newMovie.imagelink = reqs.body.imagelink
-        }
         newMovie.trailerlink = reqs.body.trailerlink
         newMovie.description = reqs.body.description
-        newMovie.actors = reqs.body.actors
+        newMovie.actors = actors
         newMovie.national = reqs.body.national
         newMovie.typemovie = type
+        if(reqs.files.image){
+            const image = await uploadFile(reqs.files.image)
+            newMovie.imagelink = image
+        }
         try{
            
             if(await Movie.findOne({'_id': reqs.params.movieId}) == null){
                 res.status(400).send("Phim không tồn tại")
             } else{
-                const movie = await Movie.findByIdAndUpdate(reqs.params.movieId, newMovie)
+                // const movie = await Movie.findByIdAndUpdate(reqs.params.movieId, newMovie)
                 res.status(201).send({"message":"Sửa phim thành công thành công"});
             } 
             
@@ -30,8 +37,7 @@ updateMovieController = async (reqs,res)=>{
             res.status(400).send(err);
         }
     }else{
-        res.status(400).send("Input đầu vào không được để trống" );
+        return res.status(400).json({"error":"tên phim hoặc link không được để trống"})
     }
-
 }
 module.exports = updateMovieController
