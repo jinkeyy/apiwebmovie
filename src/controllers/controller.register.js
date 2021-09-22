fb175d3e913a1a528d3f78c92cdb5354678dd545
jinkeyy <71177441+jinkeyy@users.noreply.github.com>
2021-09-22 17:48:40 +0700

    fix-register-user

--- src/controllers/controller.register.js
+++ src/controllers/controller.register.js
@@ -2,22 +2,26 @@ const User = require("../models/models.user")
 const express = require("express")
 const app = express()
 const bcrypt = require("bcryptjs")
-registerController = async (reqs,res)=>{
-    if(reqs.body.username && reqs.body.email && reqs.body.password ){
+registerController = async (reqs, res) => {
+    if (reqs.body.username && reqs.body.email && reqs.body.password) {
         const salt = await bcrypt.genSalt(10);
-        const hashPass = await bcrypt.hash(reqs.body.password,salt)
+        const hashPass = await bcrypt.hash(reqs.body.password, salt)
         const newUser = new User();
         newUser.username = reqs.body.username
         newUser.email = reqs.body.email
         newUser.password = hashPass
-        try{
+        try {
             const User = await newUser.save()
             res.send(User);
-        }catch(err){
-            res.status(400).send(err);
+        } catch (err) {
+            if (err.keyValue.email) {
+                res.status(400).json({ error: "Email đã được đăng kí" })
+            } else {
+                res.status(400).json(err);
+            }
         }
-    }else{
-        res.status(400).send("lỗi "+ reqs.body.email );
+    } else {
+        res.status(400).json({ error: "Chưa điền đủ thông tin" });
     }
 
 }
